ee3a71236720ae83b33fd0d6bc284f3a1d40abb9
FortEVE <76040648+FortEVE-Team@users.noreply.github.com>
2021-12-02 11:18:38 +0700

    dist

---
+++ dist/constants/natives.d.ts
@@ -0,0 +1,8 @@
+import { Binance, Ether } from '../entities/Native';
+import { ChainId } from '../enums';
+export declare const NATIVE: {
+    1: Ether;
+    3: Ether;
+    525: Ether;
+    97: Binance;
+};
